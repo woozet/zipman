47f2722bfbd56a9706c8568c8e81738abdda55f7
우준희(Junhee Woo)/Platform Architecture팀/SKP <junhee.woo@sk.com>
2016-02-26 14:18:48 +0900

    Initial Commit

---
+++ src/main/java/hello/Downloader.java
@@ -0,0 +1,73 @@
+package hello;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Created by 1001195 on 16. 2. 25..
+ */
+public class Downloader implements Runnable {
+    private static final Logger log = LoggerFactory.getLogger(Downloader.class);
+    private String url;
+    private String path;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String defaultDir) {
+        this.path = defaultDir;
+    }
+
+    private void execute() {
+        FileOutputStream fos = null;
+        InputStream is;
+        long totalBytes = 0;
+        byte[] buffer = new byte[1024];
+        int readBytes;
+
+        try {
+            fos = new FileOutputStream(this.path);
+
+            URL postUrl = new URL(this.url);
+            URLConnection urlConnection = postUrl.openConnection();
+            is = urlConnection.getInputStream();
+
+            while ((readBytes = is.read(buffer)) != -1) {
+                totalBytes += readBytes;
+                log.info("Written total : " + totalBytes + " / " + urlConnection.getContentLengthLong());
+                fos.write(buffer, 0, readBytes);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (fos != null) {
+                    fos.close();
+                }
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    @Override
+    public void run() {
+        log.info("Downloading thread is running...");
+        execute();
+        log.info("Download completed.");
+    }
+}
